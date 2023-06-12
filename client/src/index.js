4ddc8ffae11c78ee276a068d8a917fa4729b2e31
Koi-AS <lightningblade.901@gmail.com>
2023-06-11 21:35:51 -0400

    rendering react app to index.html in public

---
+++ client/src/index.js
@@ -0,0 +1,13 @@
+// import react api
+import React from 'react';
+import ReactDOM from 'react-dom';
+// import css style if we create it
+import App from './App.js';
+// import serviceWorker if we create one
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
