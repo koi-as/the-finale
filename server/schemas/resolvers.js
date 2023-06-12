bf697055a3dc93da9acffe3461869eb143e761bd
Koi-AS <lightningblade.901@gmail.com>
2023-06-11 20:56:05 -0400

    Created typeDefs and resolvers in server/schemas/

---
+++ server/schemas/resolvers.js
@@ -0,0 +1,10 @@
+// import the models and anything else we need up here
+
+// creating the resolvers
+const resolvers = {
+  // resolvers will go in here
+  // we need one object named Query: {},
+  // and another one named Mutation: {}
+};
+
+module.exports = resolvers;
