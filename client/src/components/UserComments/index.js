2f6681f716d448ec157b04ae8dc2531529d7cf58
Koi-AS <lightningblade.901@gmail.com>
2023-06-13 15:01:23 -0400

    added imports in UserComments for queries, mutations, and hooks

--- client/src/components/UserComments/index.js
+++ client/src/components/UserComments/index.js
@@ -1,15 +1,18 @@
 import React from 'react'; // import React library
-// create a function called UserComments that will
-// read the database for all the comments for a selected 
-// post then render those comments under the post.
-// It will also create a text form box for users to write
-// their own comment and post that
+import { useQuery, useMutation } from '@apollo/client'; // import useQuery and useMutation hooks
+
+// import { } from '../../utils/queries'; // import queries; get post by id, get comments by post id
+// import { } from '../../utils/mutations'; // import mutations; update, delete, create comments
+
 function UserComments() {
   return (
     <div>
       {/* create comment form */}
       <div>
-        <input className="" placeholder="Write your comment here"/>
+        <input
+          placeholder="Write your comment here"
+          value=""
+        />
       </div>
       {/* render all comments for this post */}
     </div>
