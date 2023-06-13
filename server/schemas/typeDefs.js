4dfde5e744bf0ccb84669dc0513bb778e1cb0036
Josh Claxton <125156252+bowdumb@users.noreply.github.com>
2023-06-13 12:04:53 -0400

    Commiting typedef to pull from main

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,7 +1,30 @@
 const { gql } = require('apollo-server-express');
 // create our typeDefs here
 const typeDefs = gql`
+    type User {
+        _id: ID
+        username: String
+        password: String
+        posts: [posts]
+        comments: [comments]
+    }
 
+    type Posts {
+        _id: ID
+        text: String
+        createdBy: ID
+        createdOn: String
+        comments: [comments]
+        dislikes: Number
+    }
+    
+    type: Comments {
+        _id: ID
+        text: String
+        CreatedBy: String
+        CreatedOn: String
+        post: ID
+    }
 `
 
 module.exports = typeDefs; // export typeDefs
