20a344681a379e055a395b273470da31abdc0e5f
Kevin <khampkevin@gmail.com>
2023-06-13 12:00:18 -0400

    created Post and User models

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,6 +1,6 @@
 // import mongoose
 const mongoose = require('mongoose');
 // create connection to mongoose db
-// mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/')
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/bitter')
 
 module.exports = mongoose.connection; // export mongoose connection
