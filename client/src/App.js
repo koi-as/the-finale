85394409a557ca189101d0a2a9c05d0d7c0a0d87
salzm <salmonzoor@gmail.com>
2023-06-14 11:58:32 -0400

    added some js in footer index js in components

--- client/src/App.js
+++ client/src/App.js
@@ -1,22 +1,14 @@
 // import react and apollo client
-import React from 'react'; // import react
-import {
-  BrowserRouter as Router,
-  Routes,
-  Route
-} from 'react-router-dom'; // import react router
-import {
-  ApolloClient,
-  InMemoryCache,
-  ApolloProvider
-} from '@apollo/client'; // import Apollo functions
+import React from "react"; // import react
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom"; // import react router
+import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client"; // import Apollo functions
 
-const test = "Hello Bitter! :D"
+const test = "Hello Bitter! :D";
 // import components
 
 const client = new ApolloClient({
-  cache: new InMemoryCache()
-})
+  cache: new InMemoryCache(),
+});
 
 function App() {
   return (
@@ -25,13 +17,11 @@ function App() {
       <Router>
         <div>
           {/* add a navbar here */}
-          <Routes>
-            {/* add routes here */}
-          </Routes>
+          <Routes>{/* add routes here */}</Routes>
         </div>
       </Router>
     </ApolloProvider>
-  )
+  );
 }
 
 export default App;
