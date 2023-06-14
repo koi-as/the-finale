68356248b088537190e65544ba0c81f85e5efc7b
Kevin <khampkevin@gmail.com>
2023-06-14 15:06:34 -0400

    resolvers

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,23 +1,123 @@
 // import the models and anything else we need up here
+const { AuthenticationError } = require('apollo-server-express');
 const { User, Post } = require('../models');
+const {signToken} = require('../utils/auth')
 
 // creating the resolvers
 const resolvers = {
   // resolvers will go in here
   // we need one object named Query: {},
   Query: {
-    async getPosts() {
-      try {
-        const posts = await Post.find()
-        return posts
-      } catch(err) {
-        throw new Error(err)
+    users: async () => {
+      return User.find().populate('posts')
+    },
+    user: async (parent, {username}) => {
+      return User.findOne({username}).populate('posts')
+    },
+    posts: async (parent, {username}) => {
+      const params = username ? {username} : {}
+      return Post.find(params).sort({ createdAt: -1})
+    },
+    post: async (parent, {postId}) => {
+      return Thought.findOne({ _id: postId })
+    },
+    me: async (parent, args, context) => {
+      if (context.user) {
+        return User.findOne({ _id: context.user._id }).populate('posts')
       }
+      throw new AuthenticationError('You must be logged in.')
     }
   },
   // addUser and login logic if needed later
   Mutation: {
-    
+    addUser: async (parent, { username, email, password }) => {
+      const user = await User.create({ username, email, password });
+      const token = signToken(user);
+      return { token, user };
+    },
+    login: async (parent, { email, password }) => {
+      const user = await User.findOne({ email });
+
+      if (!user) {
+        throw new AuthenticationError('No user found with this email address');
+      }
+
+      const correctPw = await user.isCorrectPassword(password);
+
+      if (!correctPw) {
+        throw new AuthenticationError('Incorrect credentials');
+      }
+
+      const token = signToken(user);
+
+      return { token, user };
+    },
+    addThought: async (parent, { body }, context) => {
+      if (context.user) {
+        const post = await Post.create({
+          body,
+          username: context.user.username,
+        });
+
+        await User.findOneAndUpdate(
+          { _id: context.user._id },
+          { $addToSet: { posts: thought._id } }
+        );
+
+        return post;
+      }
+      throw new AuthenticationError('You need to be logged in!');
+    },
+    addComment: async (parent, { postId, commentBody }, context) => {
+      if (context.user) {
+        return Post.findOneAndUpdate(
+          { _id: postId },
+          {
+            $addToSet: {
+              comments: { commentBody, username: context.user.username },
+            },
+          },
+          {
+            new: true,
+            runValidators: true,
+          }
+        );
+      }
+      throw new AuthenticationError('You need to be logged in!');
+    },
+    removePost: async (parent, { PostId }, context) => {
+      if (context.user) {
+        const post = await Post.findOneAndDelete({
+          _id: postId,
+          username: context.user.username,
+        });
+
+        await User.findOneAndUpdate(
+          { _id: context.user._id },
+          { $pull: { psots: post._id } }
+        );
+
+        return post;
+      }
+      throw new AuthenticationError('You need to be logged in!');
+    },
+    removeComment: async (parent, { postId, commentId }, context) => {
+      if (context.user) {
+        return Post.findOneAndUpdate(
+          { _id: postId },
+          {
+            $pull: {
+              comments: {
+                _id: commentId,
+                username: context.user.username,
+              },
+            },
+          },
+          { new: true }
+        );
+      }
+      throw new AuthenticationError('You need to be logged in!');
+    },
   },
 };
 
--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -3,17 +3,7 @@ const { gql } = require('apollo-server-express');
 // create our typeDefs here
 const typeDefs = gql`
 
-  type Post {
-    id: ID
-    body: String
-    username: User
-    createdAt: String
-
-  }
-
-  type Query {
-    getPosts: [Post]
-  }
+  
 `;
 
 
