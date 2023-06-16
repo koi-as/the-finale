9ce8282e6d2fe8824c0cbfc29669c1799df7c6b2
Marni Luka <marnixluka@gmail.com>
2023-06-16 13:16:22 -0400

    added lightmode button and some minor fixes here and there

---
+++ client/src/pages/LightMode.js
@@ -0,0 +1,10 @@
+import React from "react";
+import LightMode from "../components/LightMode/LightMode";
+
+export default function LightModeMischief() {
+    return(
+        <>
+        <LightMode />
+        </>
+    )
+}
