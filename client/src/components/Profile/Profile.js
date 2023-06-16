3d3fe7dbe87b9b01d21ebf322f82dd0e7af4921b
Kevin <khampkevin@gmail.com>
2023-06-16 00:58:32 -0400

    made a profile page. ***SECOND***

---
+++ client/src/components/Profile/Profile.js
@@ -0,0 +1,29 @@
+import React from "react";
+import './profile.css'
+
+export default function Profile() {
+    return (
+        <div className='container bg-slate-800 flex justify-center absolute ml-64 inset-y-20 border-2 w-8/12'>
+            <div className='feed-cont flex inline-flex flex-col'>
+                <div className="profile-title">
+                    <h1 className="profile-header text-center">Profile</h1>
+                </div>
+                <div className="about-containter text-center">
+                    <h2 className="title mb-2">About Me</h2>
+                    <textarea className="about" placeholder="About Me"></textarea>
+                </div>
+                <div className="gender-container text-center">
+                    <h2 className="title mb-0">Gender</h2>
+                    <div>
+                        <input type="radio" id="male" value="male" name="gender"/>
+                        <label className="font-semibold py-2 px-4 cursor-pointer rounded-l" for="male">Male</label>
+                        <input type="radio" id="female" value="female" name="gender" />
+                        <label className="font-semibold py-2 px-4 cursor-pointer" for="female">Female</label>
+                        <input type="radio" id="non-binary" value="non-binary" name="gender" />
+                        <label className="font-semibold py-2 px-4 cursor-pointer rounded-r" for="non-binary">Non-Binary</label>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
