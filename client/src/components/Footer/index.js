73f5e6a2258b4326a902fe1d3215e2f9ba0c7a2d
Kevin <khampkevin@gmail.com>
2023-06-17 18:44:47 -0400

    footer links open in new tab

--- client/src/components/Footer/index.js
+++ client/src/components/Footer/index.js
@@ -3,19 +3,24 @@ import { Link, useLocation, useNavigate } from "react-router-dom";
 import "./footer.css";
 
 const Footer = () => {
+
+  const openInNewTab = (url) => {
+    window.open(url, "_blank", "noreferrer")
+  }
+
   return (
     <div className="main-footer absolute inset-x-0 bottom-0 text-stone-200 w-full">
       <div className="container">
         <div className="row">
           {/* Column1 */}
-          <div className="col1">
-            <h4>Contributors:</h4>
+          <div className="col1 flex">
+            <h4>Contributors:&nbsp;</h4>
             <ul className="Linkst-unstyled">
-              <Link>|| Marni Luka ||</Link>
-              <Link> Josh Claxton ||</Link>
-              <Link> Sal Monzoor ||</Link>
-              <Link> Kevin Khamphounvong ||</Link>
-              <Link> Alex Seidensticker ||</Link>
+              <Link onClick={() => openInNewTab('https://github.com/MarniLuka')}>Marni Luka&nbsp;</Link>
+              <Link onClick={() => openInNewTab('https://github.com/claxCode')}>Josh Claxton&nbsp;</Link>
+              <Link onClick={() => openInNewTab('https://github.com/salzm')}>Sal Monzoor&nbsp;</Link>
+              <Link onClick={() => openInNewTab('https://github.com/shimpfierie')}>Kevin Khamphounvong&nbsp;</Link>
+              <Link onClick={() => openInNewTab('https://github.com/koi-as')}>Alex Seidensticker&nbsp;</Link>
             </ul>
           </div>
         </div>
